f7f7c10f00c0bf87b5658fb62b4fc40463403b28
Marius <marius.pocev@gmail.com>
2021-04-18 16:21:05 +0300

    Add log model

---
+++ Models/Log.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+namespace ElevatorAPI.Models
+{
+    public class Log
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public Elevator Elevator { get; set; }
+        public string Info { get; set; }
+    }
+}
