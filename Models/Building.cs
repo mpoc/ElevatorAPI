e3ee8d755f85414b77d089273c16f9eceefb0edd
Marius <marius.pocev@gmail.com>
2021-04-18 20:25:38 +0300

    Specify keys for building and elevator

--- Models/Building.cs
+++ Models/Building.cs
@@ -1,9 +1,11 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace ElevatorAPI.Models
 {
     public class Building
     {
+        [Key]
         public int Id { get; set; }
         public int Height { get; set; }
     }
--- Models/Elevator.cs
+++ Models/Elevator.cs
@@ -1,7 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ElevatorAPI.Models
 {
     public class Elevator
     {
+        [Key]
         public int Id { get; set; }
         public DoorStatus DoorStatus { get; set; }
         public ElevatorStatus ElevatorStatus { get; set; }
