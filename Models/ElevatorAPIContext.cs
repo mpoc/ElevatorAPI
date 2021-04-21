c018ae5d13f4fc35e41adbb78f31de958d6552f1
Marius <marius.pocev@gmail.com>
2021-04-21 19:09:54 +0300

    Specify DB relation

--- Models/ElevatorAPIContext.cs
+++ Models/ElevatorAPIContext.cs
@@ -13,5 +13,13 @@ namespace ElevatorAPI.Models
         public DbSet<Elevator> Elevators { get; set; }
         public DbSet<Building> Buildings { get; set; }
         public DbSet<Log> Logs { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Building>()
+                .HasMany(b => b.Elevators)
+                .WithOne(e => e.Building)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
     }
 }
